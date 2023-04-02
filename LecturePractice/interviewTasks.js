a61339cfed63189deccee0bf81aa612a5d754a08
Slava Morza <bloodreek@gmail.com>
2023-04-02 19:13:26 +0300

    Add 15 exe

---
+++ LecturePractice/interviewTasks.js
@@ -0,0 +1,25 @@
+//1
+let x = 5;
+console.log(x++);
+
+//2
+console.log([] + false - null + true);
+
+//3
+console.log([] + 1 + 2);
+
+//4
+console.log('1'[0]);
+
+//5
+console.log(2 && 1 && null && 0 && undefined);
+
+//6
+console.log(!!(true && true), (true && true));
+
+//7
+console.log(null || 2 && 3 || 4);
+
+//8
+console.log([1, 2, 3] === [1, 2, 3]);
+
